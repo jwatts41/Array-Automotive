b31c33a2477f6e3cca7861c4ca9407bfa56b30b1
jwatts41 <wattsjonovan@yahoo.com>
2022-08-06 13:15:22 -0400

    created a class called vehicle

---
+++ vehicleBaseClass.js
@@ -0,0 +1,9 @@
+class vehicle {
+    constructor(make, model, year, color, mileage){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.mileage = mileage;
+    }
+}
